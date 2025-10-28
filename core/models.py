e612aeef0be02821a1e5edb40851dc77f34a8bc8
Munyeshya <obedbenny54@gmail.com>
2025-10-28 22:26:58 +0200

    Createdmodels

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('client', 'Client'),
+        ('creative', 'Creative'),
+        ('admin', 'Admin'),
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=32, blank=True, null=True)
+    role = models.CharField(max_length=16, choices=ROLE_CHOICES, default='client')
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
